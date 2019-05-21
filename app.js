b3f865e30c5b74f30925108784753d8169abd20d
nathielym <nathiely@alunos.utfpr.edu.br>
2019-05-21 00:35:55 -0300

    adicionando post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let http = require('http'),
     UserDAO = require('./dao/userDAO'),
+    PostDAO = require('./dao/postDAO')
     path = require('path'),
     express = require('express'),
     app = express();
@@ -15,6 +16,12 @@ app.get('/user', (req, res) => {
     });
 });
 
+app.get('/post', (req, res) => {
+    PostDAO.get().then((posts) => {
+        res.render('post', { posts: posts } );
+    });
+});
+
 app.post('/user', (req, res) => {
     let nome = req.body.nome,
         method = req.body.method;
@@ -27,6 +34,23 @@ app.post('/user', (req, res) => {
     if (method === 'deletar') {
         UserDAO.delete(nome).then((conn) => {
             res.redirect('/user');
+        });
+    }
+});
+
+app.post('/post', (req, res) => {
+    let text = req.body.text,
+        _id = req.body._id,
+        method = req.body.method;
+    if (method === 'inserir') {
+        PostDAO.insert(text).then((conn) => {
+            res.redirect('/post');
+        });
+    }
+
+    if (method === 'deletar') {
+        PostDAO.delete(_id).then((conn) => {
+            res.redirect('/post');
         });
     }
 });
