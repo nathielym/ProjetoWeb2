2be840061b893876d9dde93b48bd1b365cab39ab
Raniel Santos <raniel@forlogic.net>
2019-05-20 22:59:46 -0300

    trab web struct

--- app.js
+++ app.js
@@ -1,43 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let http = require('http'),
+    UserDAO = require('./dao/userDAO'),
+    path = require('path'),
+    express = require('express'),
+    app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.set('views', path.join(__dirname, 'view'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/main', usersRouter);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/user', (req, res) => {
+    UserDAO.get().then((users) => {
+        res.render('user', { users: users } );
+    });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/user', (req, res) => {
+    let nome = req.body.nome,
+        method = req.body.method;
+    if (method === 'inserir') {
+        UserDAO.insert(nome).then((conn) => {
+            res.redirect('/user');
+        });
+    }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    if (method === 'deletar') {
+        UserDAO.delete(nome).then((conn) => {
+            res.redirect('/user');
+        });
+    }
 });
 
-module.exports = app;
+app.listen(3000);
