68f4d8bb1d04f4f6f3e01ff90de628fd19536077
Raniel Santos <raniel@forlogic.net>
2019-05-21 00:33:28 -0300

    user login

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
 let http = require('http'),
-    UserDAO = require('./dao/userDAO'),
-    path = require('path'),
-    express = require('express'),
-    app = express();
+  UserDAO = require('./dao/userDAO'),
+  path = require('path'),
+  express = require('express'),
+  app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'view'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/user', (req, res) => {
-    UserDAO.get().then((users) => {
-        res.render('user', { users: users } );
-    });
+  UserDAO.get().then((users) => {
+    res.render('user', { users: users });
+  });
 });
 
-app.post('/user', (req, res) => {
-    let nome = req.body.nome,
-        method = req.body.method;
-    if (method === 'inserir') {
-        UserDAO.insert(nome).then((conn) => {
-            res.redirect('/user');
-        });
-    }
 
-    if (method === 'deletar') {
-        UserDAO.delete(nome).then((conn) => {
-            res.redirect('/user');
-        });
-    }
+app.post('/user', (req, res) => {
+  let nome = req.body.nome,
+    senha = req.body.senha,
+    method = req.body.method;
+  if (method === 'inserir') {
+    // UserDAO.insert(nome, senha).then((conn) => {
+    //     res.redirect('/user');
+    // });
+    UserDAO.get().then((users) => {
+      users.forEach(function (user) {
+        if (nome == user.nome && senha == user.senha)
+          console.log("Senha correta!!");
+        else
+          console.log("Senha incorreta!!");
+          console.log(user.senha + " | " + senha);
+          console.log(user.nome + " | " + nome);
+      });
+    });
+  }
+
+  if (method === 'deletar') {
+    UserDAO.delete(nome).then((conn) => {
+      res.redirect('/user');
+    });
+  }
 });
 
 app.listen(3000);
--- dao/userDAO.js
+++ dao/userDAO.js
@@ -1,29 +1,37 @@
 let client = require('mongodb').MongoClient;
 
 module.exports = class UserDAO {
-    static connect (callback) {
-        return client.connect('mongodb://127.0.0.1:27017',
-                {useNewUrlParser: true});
-    }
 
-    static get (callback) {
-        return UserDAO.connect().then((conn) => {
-            let db = conn.db('mongo-test');
-            return db.collection('user').find().toArray();
-        });
-    }
+  static connect(callback) {
+    return client.connect('mongodb://127.0.0.1:27017',
+      { useNewUrlParser: true });
+  }
 
-    static insert (nome) {
-        return UserDAO.connect().then((conn) => {
-            let db = conn.db('mongo-test');
-            db.collection('user').insertOne({nome: nome});
-        });
-    }
+  static get(callback) {
+    return UserDAO.connect().then((conn) => {
+      let db = conn.db('mongo-test');
+      return db.collection('user').find().toArray();
+    });
+  }
 
-    static delete (nome) {
-        return UserDAO.connect().then((conn) => {
-            let db = conn.db('mongo-test');
-            db.collection('user').deleteOne({nome: nome});
-        });
-    }
+  static getByName(nome) {
+    return UserDAO.connect().then((conn) => {
+      let db = conn.db('mongo-test');
+      return db.collection('user').find({ "nome": nome });
+    });
+  }
+
+  static insert(nome, senha) {
+    return UserDAO.connect().then((conn) => {
+      let db = conn.db('mongo-test');
+      db.collection('user').insertOne({ nome: nome, senha: senha });
+    });
+  }
+
+  static delete(nome) {
+    return UserDAO.connect().then((conn) => {
+      let db = conn.db('mongo-test');
+      db.collection('user').deleteOne({ nome: nome });
+    });
+  }
 }
